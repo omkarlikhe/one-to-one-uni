e7149480c1f07fc22c1f7cf3d3ac1d384c84e47d
omkarlikhe <omkarlikhe81@gmail.com>
2022-03-28 13:01:54 +0530

    Adding Apple And Fruit

---
+++ src/main/java/com/ty/onetoone/dto/Apple.java
@@ -0,0 +1,7 @@
+package com.ty.onetoone.dto;
+
+public class Apple {
+public static void main(String[] args) {
+	System.out.println("This is Apple Code");
+}
+}
---
+++ src/main/java/com/ty/onetoone/dto/Food.java
@@ -0,0 +1,7 @@
+package com.ty.onetoone.dto;
+
+public class Food {
+	public static void main(String[] args) {
+		System.out.println("This is Food Code");
+	}
+}
